8eedb6838cd65189fa2b54d70258806b25050f28
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-01 20:03:22 +0100

    added some pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className='bg-[#F9F9F9] text-[#333333] w-full h-20 border border-black'>
-        shhhhhhhhhh
+    <nav className='bg-[#F9F9F9] text-[#333333] container max-w-full shadow-xl'>
+        <div className='flex justify-around'>
+        <h1 className='text-3xl font-semibold'>F.A's Blog</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>
+                <button>Login</button>
+            </li>
+            <li>
+                <button>Create Account</button>
+            </li>
+        </ul>
+        </div>
     </nav>
   )
 }
