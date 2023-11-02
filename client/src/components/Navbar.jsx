5699ed53430744b1f88a0b6c16f72a39cec7f5fd
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-02 19:13:10 +0100

    done with the navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <nav className="bg-[#F9F9F9] text-[#333333] container max-w-full shadow-md">
-      <div className="flex justify-around border border-red-300">
-        <h1 className="text-3xl font-semibold">F.A's Blog</h1>
-        <ul className="flex items-center font-semibold text-[17px] space-x-6">
-          <li><Link to={'/'}>Home</Link></li>
-          <li><Link to={'about'}>About</Link></li>
+      <div className="flex justify-around items-center h-[60px]">
+        <Link to={'/'} className="text-3xl font-semibold hover:underline underline-offset-4">F.A's Blog</Link>
+        <ul className="flex items-center font-semibold text-[17px] space-x-8">
+          <li className="hover:underline underline-offset-4"><Link to={'/'}>Home</Link></li>
+          <li className="hover:underline underline-offset-4"><Link to={'about'}>About</Link></li>
           <li>
             <Link to={'login'} className="button">Login</Link>
           </li>
