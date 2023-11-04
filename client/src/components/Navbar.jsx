c78d696312f94a49dce4208310fa895e20023320
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-04 14:16:09 +0100

    created the login form & made a change to navbar section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,12 +8,12 @@ export const Navbar = () => {
         <Link to={'/'} className="text-3xl font-semibold hover:underline underline-offset-4 text-[#888888]">F.A's Blog</Link>
         <ul className="flex items-center font-semibold text-[17px] space-x-8">
           <li className="hover:underline underline-offset-4"><Link to={'/'}>Home</Link></li>
-          <li className="hover:underline underline-offset-4"><Link to={'about'}>About</Link></li>
+          <li className="hover:underline underline-offset-4"><Link to={'/about'}>About</Link></li>
           <li>
-            <Link to={'login'} className="button">Login</Link>
+            <Link to={'/login'} className="button">Login</Link>
           </li>
           <li>
-            <Link to={'createAccount'} className="button">Create Account</Link>
+            <Link to={'/createAccount'} className="button">Create Account</Link>
           </li>
         </ul>
       </div>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <main>
+      <Navbar />
+      <div className="mt-28">
+        <div className="max-w-md w-full mx-auto bg-[#F9F9F9] p-8 rounded-md shadow-xl">
+          <form>
+            <div className="mb-4">
+              <label htmlFor="email" className="font-semibold text-lg">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="mt-3 w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Your Email"
+              />
+            </div>
+            <div className="mb-8">
+              <label htmlFor="password" className="font-semibold text-lg">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="mt-3 w-full p-2 border border-gray-300 rounded-md"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              type="submit"
+              className="button w-full font-semibold text-lg"
+            >
+              Login
+            </button>
+          </form>
+
+          <div className="mt-6">
+            <p className="text-sm">
+              Don't have an account?{" "}
+              <Link to={'/createAccount'} className="font-semibold">
+                Create Account
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
