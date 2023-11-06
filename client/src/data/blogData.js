316b2f805bc68e3755cdd3cc3898a7759f0d02cd
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-06 11:04:34 +0100

    working on the blogData

--- client/src/data/blogData.js
+++ client/src/data/blogData.js
@@ -1,10 +1,12 @@
-export const blogPost = [
+export const blogPosts = [
     {
         id: 1,
         image: 'https://notjustok.com/wp-content/uploads/2023/03/0_Champions-League-Group-H-Chelsea-v-Ajax-Amsterdam.jpg',
         title: 'Manchester United react to reports of the club sacking Erik Ten Hag',
-        content: `Reports surrounding the future of Erik Ten Hag surfaced on social media after Manchester United's woeful defeat at the hands of Newcastle in the Carabao Cup round of 16 fixture played on Wednesday, November 1, 2023.Reports indicate that some players in the dressing room have been unhappy with the manager's style of play with others, indicating that more players are often favoured over others. 
+        content: `Manchester United have reportedly addressed reports indicating that the club is considering sacking the current head coach, Erik Ten Hag, following the woeful results so far this season.
         
+        Reports surrounding the future of Erik Ten Hag surfaced on social media after Manchester United's woeful defeat at the hands of Newcastle in the Carabao Cup round of 16 fixture played on Wednesday, November 1, 2023.Reports indicate that some players in the dressing room have been unhappy with the manager's style of play with others, indicating that more players are often favoured over others. 
+
         According to sports journalist Fabrizio Romano, the board of directors at the club has denied the reports indicating that the head coach is being backed by the club regardless of the results.ETH, in the post-match interview after the 3-0 loss to Newcastle, spoke about his future at the club; he revealed that he is not worried about his future at the club as he is a fighter and would definitely work towards getting his side to bounce back to winning ways. He said, 'I'm a fighter and I know it's not always going up and we have a lot of setbacks this season so far, but you also have to deal with it and it's never an excuse. I have said before, when there are setbacks, the routines, the way of play are not similar, not the same, but even then you have to get the results in. Obviously, Sunday and tonight was far from that. We have to do things right, at a certain level, a minimum level to win games. I understand [the scrutiny]. When the results are not there then there is a logical process questioning that. I am confident I can do it. I have done it at all my clubs and also last year, I did it here as well. But at this moment, we are also in a bad place. I take responsibility for it. I see it as a challenge, I'm a fighter, I am in the fight and I have to make sure I share the responsibility with the players that we stick together, fight together and get better results in.'`,
         category: 'sports'
     }
