efa7f2c5877fd541959c8adeb1d8bbeeb04ae446
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-07 16:32:42 +0100

    working on the homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,12 +9,12 @@ export const HomePage = () => {
       <Navbar />
       <div>
         <h1 className='text-center mt-10 text-2xl font-bold tracking-wide text-[#888888]'>Welcome to F.A's Blog, the home of News</h1>
-        <h1 className='mt-16 ml-20 text-3xl font-bold tracking-wide text-[#888888]'>Popular Trends</h1>
-        <ul>
-          <li>All</li>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>News</li>
+        <h1 className='mt-16 ml-12 text-3xl font-bold tracking-wide text-[#888888]'>Popular Trends</h1>
+        <ul className='flex mx-12 text-lg space-x-4 my-7 cursor-pointer text-[#888888]'>
+          <li className='list'>All</li>
+          <li className='list'>Music</li>
+          <li className='list'>Sports</li>
+          <li className='list'>News</li>
         </ul>
         <div>
           <BlogContent />
