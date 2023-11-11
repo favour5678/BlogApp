7690246a6d8996d981b92cf2d52ea916ad680f10
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-11 21:57:42 +0100

    installed jwt & working on the login server

--- server/index.js
+++ server/index.js
@@ -42,9 +42,25 @@ app.post('/register', async (req, res) => {
 })
 
 
-// app.post('/login', async (req, res) => {
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
-// })
+    try {
+        const user = await UserModel.findOne({ username })
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid username or password' })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+        if(!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid username or password' })
+        }
+
+        res.status(200).json({ message: 'Login successful' })
+    } catch(error) {
+        console.error(error.message);
+    }
+})
 
 
 
