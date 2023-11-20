664342c2c8494502d2d8eb9d05a3a9658bb13fc5
Oshoaro Favour <oshoarofavour@gmail.com>
2023-11-20 15:57:12 +0100

    installed a package & working on the createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Navbar } from '../components/Navbar'
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 export const CreatePost = () => {
   return (
-    <main>
+    <main className="h-screen">
       <Navbar />
-      CreatePost
+      <form className="flex flex-col">
+        <input
+          type="text"
+          placeholder="Title"
+          className='className="mt-3 w-[60%] p-2 border border-gray-300 rounded-md'
+        />
+        <input
+          type="text"
+          placeholder="Summary"
+          className='className="mt-3 w-[60%] p-2 border border-gray-300 rounded-md'
+        />
+        <input type="file" className='className="mt-3 w-[60%] p-2 border border-gray-300 rounded-md'/>
+        <ReactQuill />
+      </form>
     </main>
-  )
-}
+  );
+};
